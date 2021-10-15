93b1e55a4ed93dbec9f099c7ebec8ecf4b73c02b
kendrickPC <kennethpchang@gmail.com>
2021-10-15 15:23:44 -0700

    Cart id put method updates properly

--- models/Cart.js
+++ models/Cart.js
@@ -4,11 +4,10 @@ const Schema = mongoose.Schema;
 
 let cartSchema = new Schema(
   {
-    userId: {type:String, required: true},
-    productId: {type:String, required: true},
-    quantity: {type:Number, required: true}
-  },
-  {timestamps: true}
+    userId: {type: String},
+    productId: {type: String},
+    quantity: {type: Number}
+  }
 )
 
 module.exports = mongoose.model("cart", cartSchema);
--- routes/cart.js
+++ routes/cart.js
@@ -4,7 +4,7 @@ const { verifyToken } = require("../validation");
 
 // /api/cart
 // Create an item into cart
-router.post("/", verifyToken ,async (req, res) => {
+router.post("/", (req, res) => {
   data = req.body;
   cart.insertMany(data)
   .then(data => { res.send(data); })
@@ -12,14 +12,34 @@ router.post("/", verifyToken ,async (req, res) => {
 })
 
 // /api/cart
-// Read all cart items
+// GET ALL cart items
 router.get("/", (req, res) => {
   cart.find()
   .then(data => { res.send(data); })
   .catch(err => { res.status(500).send( {message: err.message}); })
 })
 
+// Read specific cart item -- get 
+router.get("/:id", (req, res) => {
+  cart.findById(req.params.id)
+  .then(data => { res.send(data); })
+  .catch(err => { res.status(500).send( { message: err.message }); }) 
+})
+
 // Update cart
+router.put("/:id", (req, res) => {
+  const id = req.params.id; 
+
+  cart.findByIdAndUpdate(id, req.body)
+  .then(data => {
+    if (!data) {
+      res.status(404).send({ message: "Cannot update product with id=" + id + ". Maybe product was not found!"})
+    } else {
+      res.send( {message: "Product was successfully updated."} )
+    }
+  })
+  .catch(err => { res.status(500).send( { message: "Error updating product with id=" + id }); }) 
+})
 
 // Delete cart
 
