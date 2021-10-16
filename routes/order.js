840ba9cf481dd8fa28102000a1caaf3b1cbce777
kendrickPC <kennethpchang@gmail.com>
2021-10-16 13:23:51 -0700

    order routes completed. Swagger project completed.

--- routes/order.js
+++ routes/order.js
@@ -19,4 +19,42 @@ router.get("/", (req, res) => {
   .catch(err => { res.status(500).send( {send: err.message}); })
 })
 
+// Read specific order item --get
+router.get("/:id", (req, res) => {
+  order.findById(req.params.id)
+  .then(data => { res.send(data); })
+  .catch(err => { res.status(500).send( { message: err.message }); })
+})
+
+// Update specific order by id
+router.put("/:id", (req, res) => {
+  const id = req.params.id; 
+
+  order.findByIdAndUpdate(id, req.body)
+  .then(data => {
+    if (!data) {
+      res.status(404).send({ message: "Cannot update order with id=" + id + ". Maybe order was not found!"})
+    } else {
+      res.send( {message: "Order=" + id + " was successfully updated."} )
+    }
+  })
+  .catch(err => { res.status(500).send( { message: "Error updating order with id=" + id }); }) 
+})
+
+// Delete specific order item
+router.delete("/:id", (req, res) => {
+  const id = req.params.id; 
+
+  order.findByIdAndDelete(id)
+  .then(data => {
+    if (!data) {
+      res.status(404).send({ message: "Cannot delete order item with id=" + id + ". Maybe order item was not found!"})
+    } else {
+      res.send( {message: "Order item was successfully deleted."} )
+    }
+  })
+  .catch(err => { res.status(500).send( { message: "Error deleting order item with id=" + id }); }) 
+})
+
+
 module.exports = router;
